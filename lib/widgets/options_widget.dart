ac095df9d7d9f692fa9c1971971403b35a18f6c5
Your Name <chamidilshan55@gmail.com>
2023-03-21 12:38:22 +0530

    added answers count with result

--- lib/widgets/options_widget.dart
+++ lib/widgets/options_widget.dart
@@ -2,29 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:commerce_quiz_qpp/constants/constants.dart';
 
 class OptionCard extends StatelessWidget {
-  const OptionCard({Key? key, required this.option, required this.color, required this.onTap}) : super(key: key);
+  const OptionCard({Key? key, required this.option, required this.color}) : super(key: key);
 
   final String option;
   //final bool isClicked;
   final Color color;
-  final VoidCallback onTap;
-
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Card(
-        color: color,
-        child: ListTile(
-          title:(
-              Text(
-                option,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 14.0,
-                ),
-              )
-          ),
+    return Card(
+      color: color,
+      child: ListTile(
+        title:(
+            Text(
+              option,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14.0,
+              ),
+            )
         ),
       ),
     );
