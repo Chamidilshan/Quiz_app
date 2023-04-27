40e61c0cdaae8548279108001e7d0dc66f2ed496
Your Name <chamidilshan55@gmail.com>
2023-04-27 13:56:25 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:commerce_quiz_qpp/models/database.dart';
 import 'package:commerce_quiz_qpp/pages/starting_page.dart';
 import 'package:connectivity_wrapper/connectivity_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 import 'models/question_model.dart';
 import 'models/database.dart';
 
